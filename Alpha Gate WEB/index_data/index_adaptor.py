8c5c8c83b281685004b59066ad9421a6b6894015
dangorlov <dangorlov@yandex.ru>
2018-03-30 16:35:02 +0300

    Chonge moar tttthings)

--- Alpha Gate WEB/index_data/index_adaptor.py
+++ Alpha Gate WEB/index_data/index_adaptor.py
@@ -1,8 +1,11 @@
-def Find(query):
+def Find(query=' '):
     '''
 
     :param query: string, which describes a query
     :return: results: list like [["name", 'url', 'description']]
     '''
-    results = [["name", 'url', 'description']]
+    query = query.replace(" ", "&")
+    results = [["Задан пуской поисковый запрос.", '/', 'Эй ты! Чего запросы писать забываем?']]
+    #for i in results:
+    #    i[2]="<div>{0}</div>".format(i[2])
     return results
